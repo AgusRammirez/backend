f3e8646ba202cf0d9d2ca251ce1a78416d831c16
agustina ramirez <agusram6@gmail.com>
2022-11-03 11:15:33 -0300

    Updated

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,11 +1,51 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var novedadesModel = require("../../models/novedadesModel");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('admin/novedades', { //login.hbs
-    layout: "admin/layout",    //layout.hbs
+/* listar novedades */
+router.get("/", async function (req, res, next) {
+  var novedades = await novedadesModel.getNovedades();
+
+  res.render("admin/novedades", {
+    layout: "admin/layout",
+    usuario: req.session.nombre,
+    novedades,
   });
+});
+
+// diseño de agregar
+router.get("/agregar", (req, res, next) => {
+  res.render("admin/agregar", {
+    layout: "admin/layout",
+  });
+});
+
+// insertar novedad
+router.post("/agregar", async (req, res, next) => {
+  try {
+    console.log(req.body);
+    if (
+      req.body.titulo != "" &&
+      req.body.subtitulo != "" &&
+      req.body.descripcion != ""
+    ) {
+      await novedadesModel.insertNovedades(req.body);
+      res.redirect("/admin/novedades");
+    } else {
+      res.render("admin/agregar", {
+        layout: "admin/layout",
+        error: true,
+        message: "Todos los campos son requeridos",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.render("admin/agregar", {
+      layout: "admin/layout",
+      error: true,
+      message: "No se cargo la novedad",
+    });
+  }
 });
 
 module.exports = router;
