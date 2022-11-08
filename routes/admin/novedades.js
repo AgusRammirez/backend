6694d1f7d76f8cae8a8b7c48f4936e4c09e2f075
agustina ramirez <agusram6@gmail.com>
2022-11-08 10:23:59 -0300

    Updated

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -48,4 +48,47 @@ router.post("/agregar", async (req, res, next) => {
   }
 });
 
+// eliminar
+router.get("/eliminar/:id", async (req, res, next) => {
+  var id = req.params.id;
+
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect("/admin/novedades");
+});
+
+// modificar la vista -> formulario y datos cargados
+router.get("/modificar/:id", async (req, res, next) => {
+  var id = req.params.id;
+  // console.log(req.params.id);
+  var novedad = await novedadesModel.getNovedadesById(id);
+
+  res.render("admin/modificar", {
+    layout: "admin/layout",
+    novedad,
+  });
+});
+
+// actualizar
+router.post("/modificar", async (req, res, next) => {
+  try {
+    var obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      descripcion: req.body.descripcion,
+    };
+    // console.log(obj)
+
+    await novedadesModel.modificarNovedadesById(obj, req.body.id);
+    res.redirect("/admin/novedades");
+
+  } catch (error) {
+    console.log(error);
+    res.render("admin/modificar", {
+      layout: "admin/layout",
+      error: true,
+      message: "No se modifico la novedad",
+    });
+  }
+});
+
 module.exports = router;
