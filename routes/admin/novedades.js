20b81c4aa25965be5612c4a81e0625fce61f1eb7
agustina ramirez <agusram6@gmail.com>
2022-11-16 09:14:06 -0300

    Updated

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,10 +1,33 @@
 var express = require("express");
 var router = express.Router();
 var novedadesModel = require("../../models/novedadesModel");
+const util = require("util");
+const cloudinary = require("cloudinary").v2;
+const uploader = util.promisify(cloudinary.uploader.upload);
+const destroy = util.promisify(cloudinary.uploader.destroy);
 
 /* listar novedades */
 router.get("/", async function (req, res, next) {
   var novedades = await novedadesModel.getNovedades();
+
+  novedades = novedades.map((novedad) => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.image(novedad.img_id, {
+        width: 100,
+        height: 100,
+        crop: "fill",
+      });
+      return {
+        ...novedad,
+        imagen,
+      };
+    } else {
+      return {
+        ...novedad,
+        imagen: "",
+      };
+    }
+  });
 
   res.render("admin/novedades", {
     layout: "admin/layout",
@@ -23,13 +46,22 @@ router.get("/agregar", (req, res, next) => {
 // insertar novedad
 router.post("/agregar", async (req, res, next) => {
   try {
-    console.log(req.body);
+    var img_id = "";
+    // console.log(req.files.imagen);
+    if (req.files && Object.keys(req.files).length > 0) {
+      imagen = req.files.imagen;
+      img_id = (await uploader(imagen.tempFilePath)).public_id;
+    }
+
     if (
       req.body.titulo != "" &&
       req.body.subtitulo != "" &&
       req.body.descripcion != ""
     ) {
-      await novedadesModel.insertNovedades(req.body);
+      await novedadesModel.insertNovedades({
+        ...req.body,
+        img_id,
+      });
       res.redirect("/admin/novedades");
     } else {
       res.render("admin/agregar", {
@@ -52,6 +84,11 @@ router.post("/agregar", async (req, res, next) => {
 router.get("/eliminar/:id", async (req, res, next) => {
   var id = req.params.id;
 
+  let novedad = await novedadesModel.getNovedadesById(id);
+  if (novedad.img_id) {
+    await (destroy(novedad.img_id));
+  }
+
   await novedadesModel.deleteNovedadesById(id);
   res.redirect("/admin/novedades");
 });
@@ -71,16 +108,33 @@ router.get("/modificar/:id", async (req, res, next) => {
 // actualizar
 router.post("/modificar", async (req, res, next) => {
   try {
+    let img_id = req.body.img_original;
+    let borrar_img_vieja = false;
+
+    if (req.body.img_delete === "1") {
+      img_id = null;
+      borrar_img_vieja = true;
+    } else {
+      if (req.files && Object.keys(req.files).length > 0) {
+        imagen = req.files.imagen;
+        img_id = (await uploader(imagen.tempFilePath)).public_id;
+        borrar_img_vieja = true;
+      }
+    }
+    if (borrar_img_vieja && req.body.img_original) {
+      await destroy(req.body.img_original);
+    }
+
     var obj = {
       titulo: req.body.titulo,
       subtitulo: req.body.subtitulo,
       descripcion: req.body.descripcion,
+      img_id,
     };
-    // console.log(obj)
+    // console.log(req.body)
 
     await novedadesModel.modificarNovedadesById(obj, req.body.id);
     res.redirect("/admin/novedades");
-
   } catch (error) {
     console.log(error);
     res.render("admin/modificar", {
